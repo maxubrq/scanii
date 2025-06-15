dde83bc622f9fce2daee5f8dda6bbf2dbd737467
maxubrq <hungtp.play@gmail.com>
2025-06-15 20:42:24 +0700

    feat: exmaple hono

--- apps/upload-api/src/app.ts
+++ apps/upload-api/src/app.ts
@@ -1,3 +1,25 @@
-export const app = () => {
-  return "Hello World";
-};
+import { Hono } from 'hono';
+import { serve } from '@hono/node-server';
+
+declare global {
+  var process: {
+    env: {
+      PORT: string;
+    };
+  };
+}
+
+const app = new Hono();
+const port = Number(process.env.PORT ?? 3000);
+
+app.get('/', (c) => c.text('Hello World'));
+
+serve(
+  {
+    fetch: app.fetch,
+    port,
+  },
+  (info) => {
+    console.log(`Server is running on ${info.address}:${info.port}`);
+  },
+);
